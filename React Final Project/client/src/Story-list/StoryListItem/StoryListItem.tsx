cfa08f429a85f4e428350c7bdff03bc59060f6c1
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-11-10 14:30:03 +0200

    re doing

--- React Final Project/client/src/Story-list/StoryListItem/StoryListItem.tsx
+++ React Final Project/client/src/Story-list/StoryListItem/StoryListItem.tsx
@@ -1,29 +1,35 @@
 import "./StoryListItem.css"
 import {request} from "../../functions";
 import cache from "../../cache";
-import {chapterContext} from "../../App";
+import {chapterContext} from "../../Main/Main";
 import {useContext} from "react";
+import {Link, useNavigation} from "react-router-dom";
+
 
 export default function StoryListItem(props:any){
+
     const [chapter,setChapter]=useContext(chapterContext)
-    const bookClickHandler=(bookId:string)=>{
-        request(`books/${bookId}`).subscribe(
-            (res)=>{
-                console.log(res)
-                cache.book = res.book
-                setChapter(res.book)
-            },
-            (error)=>{
-                console.log(error)
-            }
-        )
-    }
+    // const bookClickHandler=(bookId:string)=>{
+    //     request(`books/${bookId}`).subscribe(
+    //         (res)=>{
+    //             console.log(res)
+    //             cache.book = res.book
+    //             setChapter(res.book)
+    //         },
+    //         (error)=>{
+    //             console.log(error)
+    //         }
+    //     )
+    // }
 
     return(
-        <div onClick={()=>bookClickHandler(props.storyId)} className={"item"}>
-            <img className={"storyImg"}  src={props.imgUrl}/>
-            <h3 className={"storyName"}>{props.storyName}</h3>
-            {chapter._id}
-        </div>
+        <Link to={`${props.storyId}`}>
+            <div  className={"item"}>
+                <img className={"storyImg"}  src={props.imgUrl}/>
+                <h3 className={"storyName"}>{props.storyName}</h3>
+                {chapter._id}
+            </div>
+        </Link>
+
     )
 }
