449a363ea55a280e5c038277cc1c4e3d043b3bc1
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-11-10 14:28:47 +0200

    chapter reconfig

--- React Final Project/client/src/Story/Story.tsx
+++ React Final Project/client/src/Story/Story.tsx
@@ -1,20 +1,41 @@
 import Sentence from "./Sentence/Sentence.tsx";
 import "./Story.css"
-import {useState,useEffect} from "react";
-
+import {useEffect, useContext, useState} from "react";
+// import {chapterContext} from "../Main/Main";
+import {useParams} from "react-router-dom";
+import {request} from "../functions";
 export default function Story(){
-    const[storyText,setStoryText] = useState("")
+    // const [chapter,setChapter] = useContext(chapterContext)
+    const [chapter,setChapter] = useState({
+        text:""
+    })
+
+    const {chapterId} = useParams()
+    const getChapter=(chapterId:string)=>{
+         request(`chapters/${chapterId}`).subscribe(
+             (res)=>{
+                 console.log(res)
+                 setChapter(res)
+             },
+             (error)=>{
+                 console.log(error)
+             }
+         )
+    }
 
 
-
-    const story = `
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab blanditiis repellendus ut! Adipisci alias ipsum iste quia sint! Ab aliquam commodi consequuntur illo illum ipsam iste labore laudantium mollitia nam nemo, obcaecati odio, perferendis quas quasi quia ratione, saepe sapiente sit velit. Deleniti ducimus eius ipsum laboriosam laudantium possimus, quasi repudiandae sint! Atque blanditiis, doloremque ipsam nostrum officia quas quasi sint temporibus totam voluptates. Corporis, delectus deleniti eaque ipsam laborum, officiis quidem repudiandae sint, ullam veniam voluptatem voluptatum. A accusamus accusantium aliquam asperiores aut blanditiis debitis dolore hic illum labore modi mollitia numquam odio officia quam quod reprehenderit, suscipit voluptatum.
-    `
     useEffect(()=>{
-        setStoryText(story)
-    },[])
+        console.log(1111)
+        getChapter(chapterId!)
 
-    const sentences = storyText.split(".")
+    },[chapterId])
+
+    let sentences:any =[]
+        if(chapter){
+             sentences = chapter.text.split(".")
+
+        }
+
     return(
         <div className={"story"}>
             {sentences.map((sentence,index)=><Sentence key={index}   text={sentence} />)}
