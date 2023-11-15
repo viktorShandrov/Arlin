8361934e4c9dbd485a16529d12de0a8792dccf4c
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-11-15 20:19:36 +0200

    add chapter view

--- React Final Project/client/src/admin/addChapter/AddChapterPanel.tsx
+++ React Final Project/client/src/admin/addChapter/AddChapterPanel.tsx
@@ -1,14 +1,15 @@
 import {useState} from "react";
 import {request} from "../../functions";
+import styles from "../addBook/AddBook.module.css";
 export default function AddChapterPanel(){
-    const [form,setForm] = useState({
+    const [formValues,setFormValues] = useState({
         bookId:"",
         chapterName:"",
         chapterText:"",
     })
     const onFormChange=(e:any)=>{
 
-        setForm(state=>{
+        setFormValues(state=>{
             return {
                 ...state,
                 [e.target.name]:e.target.value
@@ -17,43 +18,24 @@ export default function AddChapterPanel(){
     }
 
     const submitForm = ()=>{
-        console.log(JSON.stringify(form))
-        request("chapters/create","POST",form).subscribe(
+        // console.log(JSON.stringify(form))
+        request("chapters/create","POST",formValues).subscribe(
             (res)=>{
 
             },
-            (error)=>{
-                console.log(error)
-            }
         )
     }
     return(
         <>
-            <input
-                type="text"
-                name="bookId"
-                placeholder={"BookId"}
-                className={"bookNameInput"}
-                value={form.bookId}
-                onChange={onFormChange}
-            />
-            <input
-                type="text"
-                name="chapterName"
-                placeholder={"Chapter name"}
-                className={"chapterNameInput"}
-                value={form.chapterName}
-                onChange={onFormChange}
-            />
-            <textarea
-                name="chapterText"
-                placeholder={"Chapter text"}
-                className={"chapterTextInput"}
-                value={form.chapterText}
-                onChange={onFormChange}
-            />
-            <button className={"submitBtn"} onClick={submitForm}>Add chapter</button>
-
+            <h1>Create chapter</h1>
+            <div className={styles.formWrapper}>
+                <div className={styles.formC}>
+                    <input name={"bookId"} placeholder={"BookId"} value={formValues.bookId} onChange={onFormChange} />
+                    <input name={"chapterName"} placeholder={"Chapter name"} value={formValues.chapterName} onChange={onFormChange} />
+                    <textarea name={"chapterText"} placeholder={"Book chapterText"} value={formValues.chapterText} onChange={onFormChange} />
+                    <button onClick={submitForm} className={styles.submitBtn}>Create</button>
+                </div>
+            </div>
         </>
 
     )
