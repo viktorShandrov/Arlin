3e3c8edfc5418bc7841e9379e90e5f25b0692cbc
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-11-16 21:44:28 +0200

    main -> template for all book views

--- React Final Project/client/src/Main/Main.tsx
+++ React Final Project/client/src/Main/Main.tsx
@@ -1,70 +1,27 @@
-import StoryList from "../Story-list/StoryList";
-import Story from "../Story/Story";
-import TranslationContainer from "../TranslationContainer/TranslationContainer";
-import {Link, Route, Routes} from "react-router-dom";
-import ChapterList from "../ChapterList/ChapterList";
+import {Route, Routes} from "react-router-dom";
+import Test from "../Test/Test";
+import UnknownWords from "../UnknownWords/UnknownWords";
+import AllBooks from "../AllBooks/AllBooks";
+import BookDetails from "../BookDetails/BookDetails";
+import Read from "../Read/Read";
+import Navigation from "../Navigation/Navigation";
+
 import styles from "./Main.module.css"
-import {useContext, useState} from "react";
-import {userContext} from "../App";
-export default  function Main(){
-
-    const {user,setUser} = useContext(userContext)
-
+export default function Main(){
     return(
         <>
-            <div className={styles.navC}>
-                <nav className={styles.nav}>
-                    {user&&
-                        <>
-                            <Link to={"/user/login"} className={styles.navItem}>Read</Link>
-                            <Link to={"/user/login"} className={`${styles.navItem} ${styles.booksNav}`}>Books</Link>
-                            <div className={styles.booksMenu}>
-                                <Link to={"/main/AllBooks"} className={styles.navItem}>All books</Link>
-                                <Link to={"/user/login"} className={styles.navItem}>My books</Link>
-                            </div>
-                            <Link to={"/main/unknownWords"} className={styles.navItem}>My unknown words</Link>
-                            <Link to={"/user/login"} className={`${styles.navItem} ${styles.testsNav}`}>Tests</Link>
-                            <div className={styles.testsMenu}>
-                                <Link to={"/user/login"} className={styles.navItem}>My tests</Link>
-                                <Link to={"/main/test/randomWords"} className={styles.navItem}>Random words</Link>
-                                <Link to={"/main/test/textWords"} className={styles.navItem}>Words from text</Link>
-                                <Link to={"/main/test/textQuestions"} className={styles.navItem}>Chapter plot</Link>
-                            </div>
-                        </>
-
-                    }
-
-                    {!user&&
-                        <>
-                            <Link to={"/user/login"} className={styles.navItem}>Login</Link>
-                            <Link to={"/user/register"} className={styles.navItem}>Register</Link>
-                        </>
-
-                    }
-
-                </nav>
+            <div className={styles.templateWrapper}>
+                <Navigation />
+                <Routes>
+                    <Route path={"/test/*"} element={<Test />}></Route>
+                    <Route path={"/unknownWords"} element={<UnknownWords />}></Route>
+                    <Route path={"/AllBooks/"} element={<AllBooks />}></Route>
+                    <Route path={"/read/*"} element={<Read />}></Route>
+                    <Route path={"/AllBooks/:id"} element={<BookDetails />}></Route>
+                </Routes>
             </div>
 
-            <main className={styles.main}>
-                <Routes>
-                    <Route path={"/"} element={<StoryList />}/>
-                    <Route path={"/:bookId/:chapterId?/:textToTranslate?"} element={<ChapterList />}/>
-
-                </Routes>
-                <Routes>
-                    <Route path={"/:bookId/:chapterId/:textToTranslate?"} element={<Story />}/>
-                </Routes>
-                <Routes>
-                    <Route path={"/:bookId/:chapterId/:textToTranslate"} element={<TranslationContainer/>}/>
-                </Routes>
-
-
-            </main>
-
-
-
-         </>
-
+        </>
 
     )
 }
