fd5e6481625891ffe6208fa373290098b1f61f4c
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-12-15 17:27:00 +0200

    message login

--- React Final Project/client/src/guards/AuthGuard/AuthGuard.tsx
+++ React Final Project/client/src/guards/AuthGuard/AuthGuard.tsx
@@ -1,9 +1,11 @@
 
 import {Navigate, Outlet} from "react-router-dom";
 import {useSelector} from "react-redux";
+import {toast} from "react-toastify";
 export default function AuthGuard(){
     const {user} = useSelector((state:any)=>state.user)
     if(!user.token){
+        toast.info("Трябва да влезнеш в профил\n(You need to log in)")
         return <Navigate to={"/user/login"} />
     }
     return(
