c6c6cc5758006cfbed436144ff8e188861b67fcd
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-12-31 17:02:12 +0200

    nav for mobile

--- React Final Project/client/src/components/Navigation/Navigation.tsx
+++ React Final Project/client/src/components/Navigation/Navigation.tsx
@@ -14,7 +14,9 @@ export default function Navigation(){
             <div className={styles.currentSectionLabelC}>
                 {currentSectionName.length>0&&currentSectionName.split("").map((char,index)=><h1 key={index} className={styles.currentSectionLabelChar}>{char}</h1>)}
             </div>
-
+            <div className={styles.burgerMenuIcon}>
+                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+            </div>
 
             <section className={styles.logoC}>
                 <div className={styles.logoPicC}>
@@ -23,6 +25,7 @@ export default function Navigation(){
                 <div className={styles.logoNameC}>
                     <img src="/logo/name.png" alt=""/>
                 </div>
+
             </section>
             <section className={styles.linksC}>
                 <Link onClick={navigateHandler} to={"/main/read"} className={styles.linkItem} >
