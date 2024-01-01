a73fc4a25af06e9c2b59c8bebf2ee3e4a6afa890
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2024-01-01 17:29:25 +0200

    navigation burger click

--- React Final Project/client/src/components/Navigation/Navigation.tsx
+++ React Final Project/client/src/components/Navigation/Navigation.tsx
@@ -1,20 +1,33 @@
 
 import styles from "./Navigation.module.css"
 import {Link} from "react-router-dom";
-import {useState} from "react";
+import {useRef, useState} from "react";
 export default function Navigation(){
     const [currentSectionName,setCurrentSectionName] = useState("")
+    const navigationWrapperRef = useRef(null)
     const navigateHandler = (e:any)=>{
         setCurrentSectionName(e.target.textContent)
+        burgerMenuClickHandler()
+    }
+
+    const burgerMenuClickHandler = ()=>{
+        // @ts-ignore
+        if(navigationWrapperRef.current.classList.contains(styles.opened)){
+            // @ts-ignore
+            navigationWrapperRef.current.classList.remove(styles.opened)
+        }else{
+            // @ts-ignore
+            navigationWrapperRef.current.classList.add(styles.opened)
+        }
     }
 
     return(
-        <div className={styles.navigationWrapper}>
+        <div ref={navigationWrapperRef} className={styles.navigationWrapper}>
 
             <div className={styles.currentSectionLabelC}>
                 {currentSectionName.length>0&&currentSectionName.split("").map((char,index)=><h1 key={index} className={styles.currentSectionLabelChar}>{char}</h1>)}
             </div>
-            <div className={styles.burgerMenuIcon}>
+            <div onClick={burgerMenuClickHandler}  className={styles.burgerMenuIcon}>
                 <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
             </div>
 
