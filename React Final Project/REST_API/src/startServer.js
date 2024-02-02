2957262b33c06ea6b3ae81e43195f6884ed7ca26
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2024-02-02 22:15:49 +0200

    1

--- React Final Project/REST_API/src/startServer.js
+++ React Final Project/REST_API/src/startServer.js
@@ -1,10 +1,9 @@
 const {port} = require("./utils/utils");
 const startServer = require("./server")
 
-const app = startServer.listen(port,()=>{
+const app = startServer.listen(port, () => {
+    console.log("Server is listening on port " + port);
+});
 
-
-    console.log("Server is listening on port " + port)
-})
-
+// Set the server timeout to 60 seconds (60000 milliseconds)
 app.timeout = 60000;
