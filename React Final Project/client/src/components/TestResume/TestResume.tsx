7ba11ecf87b3f389df1260e0348f1e850b8b27d7
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2024-05-05 19:57:29 +0300

    test resume view is almost ready

--- React Final Project/client/src/components/TestResume/TestResume.tsx
+++ React Final Project/client/src/components/TestResume/TestResume.tsx
@@ -4,13 +4,14 @@ import {request} from "../../functions";
 import {useNavigate, useParams} from "react-router-dom";
 import {toast} from "react-toastify";
 import {userContext} from "../../redux/StateProvider/StateProvider";
-import {useContext, useEffect} from "react";
+import {useContext, useEffect, useState} from "react";
 // import {useDispatch} from "react-redux";
 // import {setUser} from "../../redux/user";
 // @ts-ignore
 export default function TestResume(){
     const navigate = useNavigate()
     const {testId} = useParams()
+    const [questions,setQuestions] = useState([])
     const { userState,setUserState } = useContext(userContext);
 
 
@@ -18,34 +19,189 @@ export default function TestResume(){
     useEffect(()=>{
         request(`unknownWords/testDetails/${testId}`,"GET").subscribe(
             (res:any)=>{
-                console.log(res)
+                console.log(res.testDetails.questions)
+                setQuestions(res.testDetails.questions)
             }
         )
     },[testId])
 
     return(
         <>
-            <div className={styles.questionsListC}>
-                <div className={styles.questionListItem}>
-                    <div className={styles.headings}>
-                        <p>Затвърдена е</p>
-                        <p>време за отговор 12:00 мин.</p>
-                    </div>
-                    <div className={styles.wordPair}>
-                        <span>дума</span>
-                        <p>Book</p>
-                    </div>
-                    <div className={styles.wordPair}>
-                        <span>правилен отговор</span>
-                        <p>книга</p>
+
+            <div className={styles.questionsListWrapper}>
+                <div className={styles.questionsListC}>
+                    {questions.length>0&&questions.map(question=>{
+                        if(!question.wrongAnswer.stringValue&&!question.wrongAnswer.wordId&&question.timeTaken>12){
+
+                        }else if(!question.wrongAnswer.stringValue&&!question.wrongAnswer.wordId){
+                            return <div className={`
+                            ${styles.questionListItem} 
+                            ${styles.rightAnswer} 
+                            ${question.testType=="randomWordsTests"?styles.wordType:null}
+                            ${question.testType=="fillWord"?styles.fillWordType:null}
+                            `}>
+                                <div className={styles.headings}>
+                                    <h6>Затвърдена е</h6>
+                                    <p>време за отговор <span className={styles.time}>02:00</span> мин.</p>
+                                </div>
+                                <div className={styles.wordPair}>
+                                    {question.testType=="randomWordsTests"?<p>дума</p>:null}
+                                    {question.testType=="fillWord"?<p>изречение</p>:null}
+                                    <span className={styles.word}>{question.question}</span>
+                                </div>
+                                <div className={styles.wordPair}>
+                                    <p>правилен отговор</p>
+                                    <span className={styles.word}>{question.rightAnswer.translatedText}</span>
+                                </div>
+                            </div>
+                        }else if(question.wrongAnswer.stringValue||question.wrongAnswer.wordId){
+                            return <div className={`
+                            ${styles.questionListItem} 
+                            ${styles.wrongAnswer} 
+                            ${question.testType=="randomWordsTests"?styles.wordType:null}
+                            ${question.testType=="fillWord"?styles.fillWordType:null}
+                            `}>
+                                <div className={styles.headings}>
+                                    <h6>Ще упражним още</h6>
+                                    <p>време за отговор <span className={styles.time}>02:00</span> мин.</p>
+                                </div>
+                                <div className={styles.wordPair}>
+                                    {question.testType=="randomWordsTests"?<p>дума</p>:<></>}
+                                    {question.testType=="fillWord"?<p>изречение</p>:<></>}
+                                    <span className={styles.word}>{question.question}</span>
+                                </div>
+                                <div className={styles.wrongAndRightAnswerPairC}>
+                                    <div className={styles.wrongAnswerC}>
+                                        <div className={styles.textPair}>
+                                            <div className={styles.dot}></div>
+                                            <h6 className={styles.wrongAnswerText}>{question.wrongAnswer.stringValue||question.wrongAnswer.wordId}</h6>
+                                        </div>
+                                        <h6 className={styles.answerText}>(твой отговор)</h6>
+                                    </div>
+                                    <div className={styles.rightAnswerC}>
+                                        <div className={styles.textPair}>
+                                            <div className={styles.dot}></div>
+                                            <h6 className={styles.wrongAnswerText}>{question.rightAnswer.translatedText}</h6>
+                                        </div>
+                                        <h6 className={styles.answerText}>(верен отговор)</h6>
+                                    </div>
+                                </div>
+                                <div className={styles.exampleC}>
+                                    <div className={styles.headingAndSentencePair}>
+                                        <p>пример в изречение:</p>
+                                        <div className={styles.sentenceC}>
+                                            {question.rightAnswer.examples[0].sentenceWhereWordsIsPresent.split(" ")
+                                                .map(word=>{
+                                                    return <p className={word == question.question?styles.questionedWord:styles.wordInSentence}>{word} </p>
+                                                })
+                                            }
+                                        </div>
+
+
+
+                                    </div>
+                                    <div className={styles.headingAndSentencePair}>
+                                        <p>превод на изречение:</p>
+                                        <p>{question.rightAnswer.examples[0].translation}</p>
+                                    </div>
+                                    <button className={styles.moreExamplesBtn}>още изречения</button>
+
+                                </div>
+
+                            </div>
+                        }
+
+                    })}
+
+
+
+
+                    <div className={`${styles.questionListItem} ${styles.hardQuestionAnswer} ${styles.wordType}`}>
+                        <div className={styles.headings}>
+                            <h6>Позамисли се</h6>
+                            <p>време за отговор <span className={styles.time}>02:00</span> мин.</p>
+                        </div>
+                        <div className={styles.wordPair}>
+                            <p>дума</p>
+                            <span className={styles.word}>Book</span>
+                        </div>
+                        <div className={styles.wordPair}>
+                            <p>правилен отговор</p>
+                            <span className={styles.word}>книга</span>
+                        </div>
                     </div>
 
 
+
+
+                    <div className={`${styles.questionListItem} ${styles.rightAnswer} ${styles.fillWordType}`}>
+                        <div className={styles.headings}>
+                            <h6>Затвърдена е</h6>
+                            <p>време за отговор <span className={styles.time}>02:00</span> мин.</p>
+                        </div>
+                        <div className={styles.wordPair}>
+                            <p>изречение:</p>
+                            <span className={styles.word}>The car started ________. I’m so scared.</span>
+                        </div>
+                        <div className={styles.wordPair}>
+                            <p>правилен отговор</p>
+                            <span className={styles.word}>burning</span>
+                        </div>
+                    </div>
+
+                    <div className={`${styles.questionListItem} ${styles.wrongAnswer} ${styles.fillWordType}`}>
+                        <div className={styles.headings}>
+                            <h6>Ще упражним още</h6>
+                            <p>време за отговор <span className={styles.time}>02:00</span> мин.</p>
+                        </div>
+                        <div className={styles.wordPair}>
+                            <p>изречение:</p>
+                            <span className={styles.word}>The car started ________. I’m so scared.</span>
+                        </div>
+                        <div className={styles.wrongAndRightAnswerPairC}>
+                            <div className={styles.wrongAnswerC}>
+                                <div className={styles.textPair}>
+                                    <div className={styles.dot}></div>
+                                    <h6 className={styles.wrongAnswerText}>climbing</h6>
+                                </div>
+                                <h6 className={styles.answerText}>(твой отговор)</h6>
+                            </div>
+                            <div className={styles.rightAnswerC}>
+                                <div className={styles.textPair}>
+                                    <div className={styles.dot}></div>
+                                    <h6 className={styles.wrongAnswerText}>burning</h6>
+                                </div>
+                                <h6 className={styles.answerText}>(верен отговор)</h6>
+                            </div>
+                        </div>
+                        <div className={styles.exampleC}>
+                            <div className={styles.headingAndSentencePair}>
+                                <p>превод на изречение:</p>
+                                <p>Колата започна да гори.</p>
+                            </div>
+                            <button className={styles.moreExamplesBtn}>още изречения</button>
+
+                        </div>
+
+                    </div>
+
+                    <div className={`${styles.questionListItem} ${styles.hardQuestionAnswer} ${styles.fillWordType}`}>
+                        <div className={styles.headings}>
+                            <h6>Позамисли се</h6>
+                            <p>време за отговор <span className={styles.time}>02:00</span> мин.</p>
+                        </div>
+                        <div className={styles.wordPair}>
+                            <p>изречение</p>
+                            <span className={styles.word}>The car started ________. I’m so scared.</span>
+                        </div>
+                        <div className={styles.wordPair}>
+                            <p>правилен отговор</p>
+                            <span className={styles.word}>burning</span>
+                        </div>
+                    </div>
 
                 </div>
-
             </div>
-
         </>
 
         // <div className={styles.testDoneContainer}>
